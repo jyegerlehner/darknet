caf8173a83247396eaf1901b8cb8ed2a5f757e8c
J Yegerlehner <jyegerlehner@yahoo.com>
2017-07-25 00:46:14 -0500

    a few tweaks

---
+++ test.sh
@@ -0,0 +1,1 @@
+./darknet detector test cfg/voc.data cfg/yolo-voc.cfg /media/jd/ssd120/pascal_voc/checkpoints/yolo-voc_final.weights  data/dog.jpg -thresh 0.1
---
+++ train.sh
@@ -0,0 +1,1 @@
+./darknet detector train cfg/voc.data cfg/yolo-voc.cfg /media/jd/ssd120/pascal_voc/checkpoints/yolo-voc_final.weights
